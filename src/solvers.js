4eee24440c0020e7510b092daa37fdd288d27b90
Hack Reactor Student <student@hackreactor.com>
2017-08-30 16:40:04 -0700

    implemented naive version of findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,15 +16,26 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var board = new Board({'n': n});
+  
+  for (var row = 0; row < n; row++) {
+    for (var col = 0; col < n; col++) {
+      board.get(row)[col] = 1;
+      if (board.hasRowConflictAt(row)) {
+        board.togglePiece(row, col);
+      }
+      if (board.hasColConflictAt(col)) {
+        board.togglePiece(row, col);
+      }
+    }
+  }
+  
+  return board.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
